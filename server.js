e21bc65ba6a0bb10e0cf72785cb25f836f5fe625
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-11-25 21:30:56 -0600

    beggining initial commits

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "workouts_db";
+const collections = ["workouts"];
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+    res.send(index.html);
+});
+
+app.get("/exercise", (req, res) => {
+    res.send(exercise.html);
+    // res.sendFile(path.join(_dirname, "exercise.html"))
+});
+
+// Routes
+// GET api/workouts
+// GET /exercise
+
+
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+});
