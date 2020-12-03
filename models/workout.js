db2c71b848bdd7282c39c2896468ca70ed9cdb17
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-02 19:43:06 -0500

    begining schema

--- models/workout.js
+++ models/workout.js
@@ -8,8 +8,40 @@ const workoutSchema = new Schema(
             default: () => new Date()
         },
         exercises: [
-
+                {
+                    type: {
+                        type: String,
+                        trim: true, 
+                        required: "Please enter an exercise type"
+                    },
+                    name : {
+                        type:  String,
+                        trim: true,
+                        required:  "Please enter an exercise name"
+                    },
+                    duration: {
+                        type: Number,
+                        required : "Please enter an exercise duration (in minutes)"
+                    },
+                    weight: {
+                        type: Number
+                    },
+                    reps : {
+                        type: Number
+                    },
+                    sets : {
+                        type: Number
+                    },
+                    distance : {
+                        type: Number
+                    }
+                }
         ]
+    },
+    {
+        toJSON: {
+            virtuals : true
+        }
     }
 )
 
