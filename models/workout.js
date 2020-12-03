0b09c824368968e302e7de1c17c7eb13f55cc1f2
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-02 19:21:30 -0500

    begging to build schema

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,18 @@
+const mongose = require("mongoose");
+const Schema = mongose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+
+        ]
+    }
+)
+
 
 const Workout = mongose.model("Workout", workoutSchema)
 modoule.exports = Workout
